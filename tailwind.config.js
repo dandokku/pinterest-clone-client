7b92a3bf39cb5cc6230d233bcc293ec534c96e0b
Dandokku <jesulobadaniel1@gmail.com>
2024-08-21 08:48:00 +0100

    configured tailwindcss themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      phone: '400px',
+      tablet: '625px',
+      laptop: '1000px',
+      desktop: '1115px',
+      tv: '1250px'
+    },
+    colors: {
+      'blue': '#0076D3',
+      'red': '#B60000',
+      'white': '#FFFFFF',
+      'text': '#C32F00',
+      'green': '#427B58',
+      'orange': '#ffc82c',
+      'pink': '#C31952',
+      'gray': '#628C7C',
+      'lightgray': '#E2E2E2',
+      'brown': '#C32F00',
+      'yellowish': '#FFFD92'
+    },
     extend: {},
   },
   plugins: [],
